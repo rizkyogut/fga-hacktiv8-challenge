bb301ea5646ddd86520f256ec3e4b697611ef809
Muhammad Rizky Maulana <98185052+rizkyogut@users.noreply.github.com>
2023-04-10 21:54:25 +0700

    deploy to railway

--- project1/config/gorm.go
+++ project1/config/gorm.go
@@ -53,12 +53,7 @@ func (p *Gorm) OpenConnection() error {
 	// init dsn
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", p.Address, p.Port, p.Username, p.Password, p.Database)
 
-	dbConnection, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		// NamingStrategy: schema.NamingStrategy{
-		// 	TablePrefix:   "db_go_sql.",
-		// 	SingularTable: false,
-		// },
-	})
+	dbConnection, err := gorm.Open(postgres.Open(dsn))
 	if err != nil {
 		return err
 	}
